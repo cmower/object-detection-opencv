e2f66d8a006be6b8c7040fd9847b4f85f0b1356b
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-05-18 11:55:15 +0200

    Package yolo_opencv

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="yolo_opencv",
+    description="YOLO Object detection with OpenCV and Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="1.0.0",
+    author="Arun Ponnusamy, Christopher E. Mower",
+    author_email="christopher.mower@kcl.ac.uk",
+    url="https://github.com/cmower/object-detection-opencv",
+    packages=["yolo_opencv"],
+    license="MIT License",
+    entry_points={
+        "console_scripts": [
+            "yolo_opencv = yolo_opencv.yolo_opencv:main",
+        ],
+    },
+    install_requires=[
+        "opencv-contrib-python",
+        "numpy",
+        "matplotlib",
+    ],
+)
